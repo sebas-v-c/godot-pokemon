48443228e9bd8824f23ebce2596e71f9d6ce16b8
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2021-08-15 22:11:18 -0600

    Add animation player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,9 +4,14 @@ extends KinematicBody2D
 export var walk_speed = 4.0
 const TILE_SIZE = 16
 
+onready var anim_tree = $AnimationTree
+onready var anim_state = anim_tree.get("parameters/playback")
+
 var initial_position = Vector2(0, 0)
 var input_direction = Vector2(0, 0)
 var is_moving = false
+# This variable is more like a product between two numbers that results in less
+# tan one. So it is used to multiply by the tile size and produce movement.
 var percent_moved_to_next_tile = 0.0
 
 
@@ -19,7 +24,9 @@ func _physics_process(delta):
 		process_player_input()
 	elif input_direction != Vector2.ZERO:
 		move(delta)
+		anim_state.travel("Walk")
 	else:
+		anim_state.travel("Idle")
 		is_moving = false
 
 
@@ -30,8 +37,12 @@ func process_player_input() -> void:
 		input_direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 		
 	if input_direction != Vector2.ZERO:
+		anim_tree.set("parameters/Idle/blend_position", input_direction)
+		anim_tree.set("parameters/Walk/blend_position", input_direction)
 		initial_position = position
 		is_moving = true
+	else:
+		anim_state.travel("Idle")
 
 
 func move(delta) -> void:
